3cd103005397b669bbcff76fc094709c4da43bc8
masamichiyama <masamichi.yama@gmail.com>
2025-11-11 23:19:09 +0900

    Update script.js

--- script.js
+++ script.js
@@ -10,10 +10,10 @@ document.getElementById('workoutForm').addEventListener('submit', function(e) {
   var type = document.querySelector('input[name="type"]:checked').value;
   
   var formData = new FormData();
+  formData.append('type', type);
   formData.append('date', date);
   formData.append('weight', weight);
   formData.append('reps', reps);
-  formData.append('type', type);
   
 <!--Google Apps ScriptのウェブアプリURLを指定します-->
   fetch('https://script.google.com/macros/s/AKfycbw7qLUoQQy6a7aUj-Sp0SqNOn7sAoHmgXcOZTpcuE2NhM98EZAe1JAT18FpVot9cKFX/exec', {
